c2853c4b047779d036968f01efbc62166d7d0287
sherife mourad <id.mourad@yahoo.fr>
2013-04-17 09:27:10 +0000

    added presentaion couche

--- src/main/java/onda/User.java
+++ src/main/java/onda/User.java
@@ -1,16 +1,3 @@
-/**
- * "Visual Paradigm: DO NOT MODIFY THIS FILE!"
- * 
- * This is an automatic generated file. It will be regenerated every time 
- * you generate persistence class.
- * 
- * Modifying its content may cause the program not work, or your work may lost.
- */
-
-/**
- * Licensee: DuKe TeAm
- * License Type: Purchased
- */
 package onda;
 
 public class User {
